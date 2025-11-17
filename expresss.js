a1445d56abbc83ad484f13a918cb281a1dbb14ee
developer <qa@uppercliff.com>
2025-11-17 09:20:55 +0530

    first commit

---
+++ expresss.js
@@ -0,0 +1,17 @@
+//const express = require("express");
+//const app = express();
+
+// This route will load in the browser
+/*app.get("/", (req, res) => {
+  res.send("<h1>Hello from Expres!</h1>");
+});*/
+
+// Start server
+/*app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});*/
+//console.log("Node app running!");
+// dispayName('fortune4');
+// function dispayName(name) {
+// console.log("Hello !"+name);
+// }
